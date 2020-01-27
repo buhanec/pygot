9291ebbfa8e311a6d738b10c27b668538b380228
Alen <alen.buhanec@gmail.com>
2020-01-27 15:49:20 +0000

    Add House and HouseInfo examples

--- pygot/models/__init__.py
+++ pygot/models/__init__.py
@@ -2,9 +2,53 @@
 
 from __future__ import annotations
 
-__all__ = ('map', 'player', 'cards', 'board')
+__all__ = ('House', 'HouseInfo',)
 
-map = None
-player = None
-cards = None
-board = None
+from dataclasses import dataclass
+from enum import Enum
+
+
+@dataclass(frozen=True)
+class HouseInfo:
+    """House information."""
+
+    name: str
+    words: str
+    description: str = ''
+
+
+_STARK = HouseInfo(name='Stark',
+                   words='Winter is Coming')
+_GREYJOY = HouseInfo(name='Greyjoy',
+                     words='We Do Not Sow')
+_LANNISTER = HouseInfo(name='Lannister',
+                       words='Hear Me Roar')
+_MARTELL = HouseInfo(name='Martell',
+                     words='Unbowed, Unbent, Unbroken')
+_TYRELL = HouseInfo(name='Tyrell',
+                    words='Growing String')
+_BARATHEON = HouseInfo(name='Baratheon',
+                       words='Ours is the Fury')
+
+
+class House(Enum):
+    """Houses of Westeros."""
+
+    STARK = _STARK
+    GREYJOY = _GREYJOY
+    LANNISTER = _LANNISTER
+    MARTELL = _MARTELL
+    TYRELL = _TYRELL
+    BARATHEON = _BARATHEON
+
+    @property
+    def info(self) -> HouseInfo:
+        """
+        Get information about house.
+
+        :return: Information about house
+        """
+        return self.value
+
+    def __repr__(self) -> str:
+        return f'<House.{self.name}>'
---
+++ tests/test_models.py
@@ -0,0 +1,25 @@
+"""Test models."""
+
+from __future__ import annotations
+
+import pytest
+
+from pygot import models
+
+
+@pytest.mark.parametrize('house', (
+        'baratheon',
+        'greyjoy',
+        'lannister',
+        'martell',
+        'stark',
+        'tyrell',
+))
+def test_houses_set_up(house: str) -> None:
+    if not hasattr(models.House, house.upper()):
+        raise AssertionError(f'Missing house {house!r}')
+    house_enum: models.House = getattr(models.House, house.upper())
+    if not house_enum.info.name:
+        raise AssertionError(f'House {house!r} missing name')
+    if not house_enum.info.words:
+        raise AssertionError(f'Houes {house!r} missing words')
