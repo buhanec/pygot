07c336e0bdbb0dc0b1c17752965196c9cc058d47
Sophie <sophiemltan@gmail.com>
2020-01-30 00:00:04 +0000

    Simple server example API

--- pygot/server.py
+++ pygot/server.py
@@ -2,12 +2,70 @@
 
 from __future__ import annotations
 
-__all__ = ('main',)
+from http import HTTPStatus
+from typing import Optional
+
+from flask import Flask, Response, jsonify
+
+from pygot.models import House
+from pygot.models.serialisation import JSON, JSONType
+
+app = Flask(__name__)
+
+__all__ = tuple()
 
 
-def main() -> None:
-    """Run server."""
+class APIException(Exception):
+    """Generic API exception."""
+
+    def __init__(self,
+                 reason: str,
+                 code: HTTPStatus = HTTPStatus.INTERNAL_SERVER_ERROR,
+                 extra_info: Optional[JSONType] = None) -> None:
+        super().__init__(f'{code} - {reason}')
+        self.reason = reason
+        self.code = code
+        self.extra_info = extra_info
+
+    def to_json(self) -> JSON:
+        return {
+            'code': self.code.value,
+            'message': self.code.phrase,
+            'reason': self.reason,
+            'extra': self.extra_info,
+        }
+
+
+@app.route('/')
+def index():
+    return jsonify({
+        '/house/<house_name>': 'Get house information'
+    })
+
+
+@app.route('/house/<house_name>')
+def get_house(house_name: str):
+    """
+    Get house information.
+
+    :param house_name: House name
+    :return: House information
+    """
+    try:
+        house = House[house_name.upper()]
+    except KeyError as e:
+        raise APIException(reason='House not found',
+                           code=HTTPStatus.NOT_FOUND,
+                           extra_info={'houseName': house_name}) from e
+    return jsonify(house.info.to_json())
+
+
+@app.errorhandler(APIException)
+def handle_invalid_usage(error: APIException):
+    response: Response = jsonify(error.to_json())
+    response.status_code = error.code
+    return response
 
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
