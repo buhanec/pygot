f47d8902c7dab7e6970f84cb0304d6ce38e6d5be
Alen <alen.buhanec@gmail.com>
2020-02-08 02:02:34 +0000

    Add server

--- pygot/server.py
+++ pygot/server.py
@@ -2,12 +2,22 @@
 
 from __future__ import annotations
 
-__all__ = ('main',)
+__all__ = tuple()
 
+import logging
 
-def main() -> None:
-    """Run server."""
+from starlette.applications import Starlette
+from starlette.config import Config
+import uvicorn
+
+logger = logging.getLogger(__name__)
+
+app = Starlette(debug=Config('.env').get('DEBUG', bool, False))
 
 
 if __name__ == '__main__':
-    main()
+    uvicorn.run(f'{__name__}:app',
+                host='127.0.0.1',
+                port=5000,
+                log_level='debug',
+                reload=Config('.env').get('DEBUG', bool, False))
